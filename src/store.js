a1fee119cebdb1f3caf30d7010ac73c004996632
Elio Lu <1520622848@qq.com>
2021-10-02 19:44:35 +0800

    upd: redux-thunk and style

--- src/store.js
+++ src/store.js
@@ -1,7 +1,9 @@
-import { createStore, combineReducers } from "redux"
+import { createStore, combineReducers, applyMiddleware } from "redux"
 import { persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2"
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { todos } from './todos/reducers'
 
 const reducers = {
@@ -17,11 +19,13 @@ const persistConfig = {
 const rootReducer = combineReducers(reducers)
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-const configureStore = () => 
+// TODO use thunk for actions ?
+const configureStore = () =>
     createStore(
         persistedReducer,
-        window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__(),
+        composeWithDevTools(
+            applyMiddleware(thunk)
+        )
     )
 
 export default configureStore
